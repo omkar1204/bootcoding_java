5bf1059c914d1766c2790c7ae9d03c6564cebc1f
omkar1204 <111444912+omkar1204@users.noreply.github.com>
2024-06-12 12:08:49 +0530

    Update ArrayReverse.java

--- bootcoding_java/src/com/bootcoding/basic/ArrayReverse.java
+++ bootcoding_java/src/com/bootcoding/basic/ArrayReverse.java
@@ -5,6 +5,7 @@ public class ArrayReverse {
         int a[] = {5, 7, 8, 11, 13,34,58};
 
         int n = a.length;
+        
         for (int i = 0; i<n; i++){
             System.out.print(a[i] + " ");
         }
